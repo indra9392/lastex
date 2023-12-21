cacb521aa3fa760c9cb7ca14f14a2a16a721bf3f
ex84 <ex84@RP21SYS4.win2k18.local>
2023-12-21 13:41:37 +0530

    commmit

---
+++ src/main/java/mavex/lastex/Add.java
@@ -0,0 +1,9 @@
+package mavex.lastex;
+
+public class Add {
+	
+	int add(int a,int b) {
+		return a+b;
+	}
+
+}
--- src/main/java/mavex/lastex/App.java
+++ src/main/java/mavex/lastex/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Add a=new Add();
+        a.add(10, 20);
     }
 }
